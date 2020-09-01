8bd1076b37f0f0107690822b4d91797698abe075
dreamxinxcode <dreamxinxcode@gmail.com>
2020-09-01 15:42:55 -0700

    rendering daylist in app

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,6 +3,25 @@ import React from 'react';
 import DayListItem from './DayListItem';
 
 
+const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+];
+  
+
 export default function DayList(props) {
     
     let days = props.days.map(day =>
