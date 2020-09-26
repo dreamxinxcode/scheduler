7f6e65436cc93bd102e9f24cbdb9fb97a89d7f76
dreamxinxcode <dreamxinxcode@gmail.com>
2020-09-26 16:04:05 -0700

    added screenshots

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -1,20 +1,16 @@
-import React from 'react';
-
-import DayListItem from './DayListItem';
-  
+import React from "react";
+import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-    
-    let days = props.days.map(day =>
-        <DayListItem 
-        key={day.id}
-        name={day.name} 
-        spots={day.spots} 
-        selected={day.name === props.day}
-        setDay={props.setDay} />
-    )
+  let days = props.days.map((day) => (
+    <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay}
+    />
+  ));
 
-    return (
-        <ul>{days}</ul>
-    );
+  return <ul>{days}</ul>;
 }
--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import classnames from "classnames";
 import "components/InterviewerListItem.scss";
-import InterviewerList from "./InterviewerList";
-
 
 export default function InterviewerListItem(props) {
   const interviewersClass = classnames("interviewers__item", {
     "interviewers__item--selected": props.selected,
   });
-
-
 
   return (
     <li onClick={props.setInterviewer} className={interviewersClass}>
